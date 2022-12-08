44ccd78c1903de8b2dd0af4cd166f22bc43e0fea
Lando de Weerdt <lando.deweerdt@gmail.com>
2022-12-08 19:54:19 +0100

    Add comments

--- build/CMakeFiles/OOPDeepLearning.dir/DependInfo.cmake
+++ build/CMakeFiles/OOPDeepLearning.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/lando/CppND-Capstone-Hello-World/src/main.cpp" "/home/lando/CppND-Capstone-Hello-World/build/CMakeFiles/OOPDeepLearning.dir/src/main.cpp.o"
+  "/home/lando/OOPDeepLearningCPP/src/main.cpp" "/home/lando/OOPDeepLearningCPP/build/CMakeFiles/OOPDeepLearning.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/lando/CppND-Capstone-Hello-World
+# Install script for directory: /home/lando/OOPDeepLearningCPP
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/lando/CppND-Capstone-Hello-World/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/lando/OOPDeepLearningCPP/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
